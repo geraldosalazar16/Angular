b9fcf0ccc17fc86af47a73b910dca058b53db0ab
Administrador@imnc.org.mx <Administrador@imnc.org.mx>
2018-02-26 15:37:58 -0600

    Primer codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myData:Array<any>;
+  constructor(){
+    this.myData = [
+      {
+        "albumId": 1,
+        "id": 1,
+        "title": "Imagen 1",
+        "url": "http://apinube.com/Lenna.png",
+        "thumbnailUrl": "http://placehold.it/150/92c952"
+      },
+      {
+        "albumId": 1,
+        "id": 2,
+        "title": "Imagen 2",
+        "url": "http://apinube.com/Lenna.png",
+        "thumbnailUrl": "http://placehold.it/150/771796"
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
